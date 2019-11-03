000dd79fb55dbd265550c4d201d463afa41ddba1
Roger Howell <howelrtc@aston.ac.uk>
2019-11-03 09:39:56 +0000

    Added configuration options to allow changing of wasd vs arrow keys;

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,8 @@
+// Reference Values
 const KEYCODE_PAGE_UP = 33;
 const KEYCODE_PAGE_DOWN = 34;
+const KEYCODE_Q = 81;
+const KEYCODE_E = 69;
 const KEYCODE_W = 87;
 const KEYCODE_A = 65;
 const KEYCODE_S = 83;
@@ -8,6 +11,24 @@ const KEYCODE_ARROW_LEFT = 37;
 const KEYCODE_ARROW_RIGHT = 39;
 const KEYCODE_ARROW_UP = 38;
 const KEYCODE_ARROW_DOWN = 40;
+
+
+// Configuration Options:
+const PLAYER_UP = KEYCODE_ARROW_UP;
+const PLAYER_DOWN = KEYCODE_ARROW_DOWN;
+const PLAYER_RIGHT = KEYCODE_ARROW_RIGHT;
+const PLAYER_LEFT = KEYCODE_ARROW_LEFT;
+
+const CAMERA_UP = KEYCODE_W;
+const CAMERA_DOWN = KEYCODE_S;
+const CAMERA_RIGHT = KEYCODE_D;
+const CAMERA_LEFT = KEYCODE_A;
+
+// const ZOOM_IN = KEYCODE_Q;
+// const ZOOM_DOWN = KEYCODE_E;
+const ZOOM_IN = KEYCODE_PAGE_UP;
+const ZOOM_DOWN = KEYCODE_PAGE_DOWN;
+
 
 class Game {
     public static CAMERA_SPEED = 0.4;
@@ -38,21 +59,21 @@ class Game {
     public tick(dt: number, keys: { [k: number]: number }): void {
         let dc = dt * Game.CAMERA_SPEED;
         this.camera.translate(
-            (keys[KEYCODE_ARROW_RIGHT] - keys[KEYCODE_ARROW_LEFT]) * dc, // right - left
-            (keys[KEYCODE_ARROW_DOWN] - keys[KEYCODE_ARROW_UP]) * dc, // down - up
+            (keys[CAMERA_RIGHT] - keys[CAMERA_LEFT]) * dc, // right - left
+            (keys[CAMERA_DOWN] - keys[CAMERA_UP]) * dc, // down - up
         );
 
         this.handleKeyPresses(dt, keys);
     }
 
     private handleKeyPresses(dt: number, keys: { [k: number]: number }) {
-        let dz = (keys[KEYCODE_PAGE_UP] - keys[KEYCODE_PAGE_DOWN]) * dt * Game.CAMERA_ZOOM_SPEED;
+        let dz = (keys[ZOOM_IN] - keys[ZOOM_DOWN]) * dt * Game.CAMERA_ZOOM_SPEED;
         this.camera.scale = Util.limitNumberRange(this.camera.scale + dz, Game.MIN_ZOOM, Game.MAX_ZOOM);
 
         const dxy = Game.PLAYER_SPEED * dt;
         this.movePlayerWithinBounds(
-            (keys[KEYCODE_D] - keys[KEYCODE_A] - keys[KEYCODE_S] + keys[KEYCODE_W]) * dxy,
-            (keys[KEYCODE_D] - keys[KEYCODE_A] + keys[KEYCODE_S] - keys[KEYCODE_W]) * dxy
+            (keys[PLAYER_RIGHT] - keys[PLAYER_LEFT] - keys[PLAYER_DOWN] + keys[PLAYER_UP]) * dxy,
+            (keys[PLAYER_RIGHT] - keys[PLAYER_LEFT] + keys[PLAYER_DOWN] - keys[PLAYER_UP]) * dxy
         );
     }
 
