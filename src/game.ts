7550daf186a6e19f552a6e80eee8a81b03e77970
Roger Howell <howelrtc@aston.ac.uk>
2019-11-03 06:10:03 +0000

    minor refactor;

--- src/game.ts
+++ src/game.ts
@@ -24,12 +24,28 @@ class Game {
             (keys[40] - keys[38]) * dc, // down - up
         );
 
+        this.handleKeyPresses(dt, keys);
+    }
+
+    private handleKeyPresses(dt: number, keys: { [k: number]: number }) {
         let dz = dt * Game.CAMERA_ZOOM_SPEED;
-        if (keys[KEYCODE_PAGE_UP]) { this.camera.scale = Util.limitNumberRange(this.camera.scale + dz, Game.MIN_ZOOM, Game.MAX_ZOOM); } // page up/zoom in
-        if (keys[KEYCODE_PAGE_DOWN]) { this.camera.scale = Util.limitNumberRange(this.camera.scale - dz, Game.MIN_ZOOM, Game.MAX_ZOOM); } // page down/zoom out
+        if (keys[KEYCODE_PAGE_UP]) {
+            this.camera.scale = Util.limitNumberRange(this.camera.scale + dz, Game.MIN_ZOOM, Game.MAX_ZOOM);
+        } // page up/zoom in
+        if (keys[KEYCODE_PAGE_DOWN]) {
+            this.camera.scale = Util.limitNumberRange(this.camera.scale - dz, Game.MIN_ZOOM, Game.MAX_ZOOM);
+        } // page down/zoom out
 
         const stepSize = 0.1;
-        if (keys[KEYCODE_A]) { this.movePlayerWithinBounds(SCENE_DATA.player, -stepSize, -stepSize); } else if (keys[KEYCODE_D]) { this.movePlayerWithinBounds(SCENE_DATA.player, +stepSize, +stepSize); } else if (keys[KEYCODE_W]) { this.movePlayerWithinBounds(SCENE_DATA.player, +stepSize, -stepSize); } else if (keys[KEYCODE_S]) { this.movePlayerWithinBounds(SCENE_DATA.player, -stepSize, +stepSize); }
+        if (keys[KEYCODE_A]) {
+            this.movePlayerWithinBounds(SCENE_DATA.player, -stepSize, -stepSize);
+        } else if (keys[KEYCODE_D]) {
+            this.movePlayerWithinBounds(SCENE_DATA.player, +stepSize, +stepSize);
+        } else if (keys[KEYCODE_W]) {
+            this.movePlayerWithinBounds(SCENE_DATA.player, +stepSize, -stepSize);
+        } else if (keys[KEYCODE_S]) {
+            this.movePlayerWithinBounds(SCENE_DATA.player, -stepSize, +stepSize);
+        }
     }
 
     private movePlayerWithinBounds(player: Player, dx: number, dy: number) {
