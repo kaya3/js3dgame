a00ebe68ed453a07116db93f5d0a45340d102859
Roger Howell <howelrtc@aston.ac.uk>
2019-11-03 09:46:48 +0000

    updated of map geometry;

--- src/scene-data.ts
+++ src/scene-data.ts
@@ -19,6 +19,7 @@ const SCENE_DATA = (function (): SceneData {
         return new Vector3(x, y, z);
     }
 
+    const Z_GROUND = 0;
     const Z_PLAT1 = 0;
     const Z_PLAT2 = 2.5;
 
@@ -26,24 +27,24 @@ const SCENE_DATA = (function (): SceneData {
         faces: [
             // Room 1
             { label: "C", isWalkable: true, texture: "floor", coords: [v(0, 0, Z_PLAT1), v(6, 0, Z_PLAT1), v(6, 10, Z_PLAT1), v(0, 10, Z_PLAT1)] },
-            { label: "A", isWalkable: false, texture: "wall", coords: [v(0, 0, 0), v(0, 0, 1), v(6, 0, 1), v(6, 0, 0)] },
-            { label: "B", isWalkable: false, texture: "wall", coords: [v(6, 10, 1), v(6, 10, 0), v(6, 0, 0), v(6, 0, 1)] },
-            { label: "D", isWalkable: false, texture: "wall", coords: [v(0, 0, 0), v(0, 0, 1), v(0, 10, 1), v(0, 10, 0)] },
-            { label: "E", isWalkable: false, texture: "wall", coords: [v(0, 10, 0), v(0, 10, 1), v(2, 10, 1), v(2, 10, 0)] },
-            { label: "F", isWalkable: false, texture: "wall", coords: [v(4, 10, 0), v(4, 10, 1), v(6, 10, 1), v(6, 10, 0)] },
+            { label: "A", isWalkable: false, texture: "wall", coords: [v(0, 0, Z_GROUND), v(0, 0, 1), v(6, 0, 1), v(6, 0, Z_GROUND)] },
+            { label: "B", isWalkable: false, texture: "wall", coords: [v(6, 10, 1), v(6, 10, Z_GROUND), v(6, 0, Z_GROUND), v(6, 0, 1)] },
+            { label: "D", isWalkable: false, texture: "wall", coords: [v(0, 0, Z_GROUND), v(0, 0, 1), v(0, 10, 1), v(0, 10, Z_GROUND)] },
+            { label: "E", isWalkable: false, texture: "wall", coords: [v(0, 10, Z_GROUND), v(0, 10, 1), v(2, 10, 1), v(2, 10, Z_GROUND)] },
+            { label: "F", isWalkable: false, texture: "wall", coords: [v(4, 10, Z_GROUND), v(4, 10, 1), v(6, 10, 1), v(6, 10, Z_GROUND)] },
 
             // Corridor
             { label: "O", isWalkable: true, texture: "floor", coords: [v(2, 10, Z_PLAT1), v(4, 10, Z_PLAT1), v(4, 15, Z_PLAT2), v(2, 15, Z_PLAT2)] },
-            { label: "G", isWalkable: false, texture: "wall", coords: [v(4, 10, 0), v(4, 10, 1), v(4, 15, Z_PLAT2), v(4, 15, 1)] },
-            { label: "H", isWalkable: false, texture: "wall", coords: [v(2, 10, 0), v(2, 10, 1), v(2, 15, Z_PLAT2), v(2, 15, 0)] },
+            { label: "G", isWalkable: false, texture: "wall", coords: [v(4, 10, Z_GROUND), v(4, 10, 1), v(4, 15, Z_PLAT2), v(4, 15, 1)] },
+            { label: "H", isWalkable: false, texture: "wall", coords: [v(2, 10, Z_GROUND), v(2, 10, 1), v(2, 15, Z_PLAT2), v(2, 15, Z_GROUND)] },
 
             // Room 2
             { label: "N", isWalkable: true, texture: "floor", coords: [v(-4, 15, Z_PLAT2), v(6, 15, Z_PLAT2), v(6, 23, Z_PLAT2), v(-4, 23, Z_PLAT2)] },
-            { label: "I", isWalkable: false, texture: "wall", coords: [v(-4, 15, 1), v(-4, 15, Z_PLAT2), v(2, 15, Z_PLAT2), v(2, 15, 1)] },
-            { label: "J", isWalkable: false, texture: "wall", coords: [v(-4, 23, 0), v(-4, 23, Z_PLAT2), v(-4, 15, Z_PLAT2), v(-4, 15, 0)] },
-            { label: "K", isWalkable: false, texture: "wall", coords: [v(-4, 23, 0), v(-4, 23, Z_PLAT2), v(6, 23, Z_PLAT2), v(6, 23, 0)] },
-            { label: "L", isWalkable: false, texture: "wall", coords: [v(6, 15, 1), v(6, 15, Z_PLAT2), v(6, 23, Z_PLAT2), v(6, 23, 1)] },
-            { label: "M", isWalkable: false, texture: "wall", coords: [v(4, 15, 1), v(4, 15, Z_PLAT2), v(6, 15, Z_PLAT2), v(6, 15, 1)] },
+            { label: "I", isWalkable: false, texture: "wall", coords: [v(-4, 15, Z_GROUND), v(-4, 15, Z_PLAT2), v(2, 15, Z_PLAT2), v(2, 15, Z_GROUND)] },
+            { label: "J", isWalkable: false, texture: "wall", coords: [v(-4, 15, Z_GROUND), v(-4, 15, Z_PLAT2), v(-4, 23, Z_PLAT2), v(-4, 23, Z_GROUND), ] },
+            { label: "K", isWalkable: false, texture: "wall", coords: [v(-4, 23, Z_GROUND), v(-4, 23, Z_PLAT2), v(6, 23, Z_PLAT2), v(6, 23, Z_GROUND)] },
+            { label: "L", isWalkable: false, texture: "wall", coords: [v(6, 15, Z_GROUND), v(6, 15, Z_PLAT2), v(6, 23, Z_PLAT2), v(6, 23, Z_GROUND)] },
+            { label: "M", isWalkable: false, texture: "wall", coords: [v(4, 15, Z_GROUND), v(4, 15, Z_PLAT2), v(6, 15, Z_PLAT2), v(6, 15, Z_GROUND)] },
         ],
 
         lights: [
